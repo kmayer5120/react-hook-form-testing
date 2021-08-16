eab2effaf938813c15fe52bc821b820dec14e619
Kyle Mayer <kylemayer5120@gmail.com>
2021-08-15 21:49:41 -0600

    Remove unused import and add basic title to home.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Testing out react-hook-form and yup</h2>
         <Form />
       </header>
     </div>
